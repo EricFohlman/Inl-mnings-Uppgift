becb057db98f2ad7c734ee9d39e8c7deecd2123e
Elev <Elev@TEB127025>
2019-09-13 09:31:31 +0200

    färdigställt koden

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -6,7 +6,45 @@ namespace Inlämningsuppgift1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int compnum = rnd.Next(1, 100);
+            bool status = true;
+            int usercount = 0;
+
+            Console.WriteLine("gissa ett nummer mellan 1 och 100");
+            
+            while (status)
+            {
+                usercount ++ ;
+
+                Console.WriteLine("Skriv ett tal: ");
+                string input = Console.ReadLine();
+                int.TryParse(input, out int usernum);
+                
+                if (usernum == compnum)
+                {
+                    Console.WriteLine("Du gissade rätt!");
+                    Console.WriteLine("Antal gissningar: " + usercount);
+                    status = false;
+                }
+
+                else
+                {
+                     if (usernum < compnum)
+                     {
+                        Console.WriteLine("Talet är större!");
+                     }
+                     else if (usernum > compnum)
+                     {
+                        Console.WriteLine("Talet är mindre!");
+                     }
+
+                }
+                
+                
+            }
+
         }
     }
 }
+
